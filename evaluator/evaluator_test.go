09fe8d2f6782891bd35ff0ae31d516accabcb27b
khongchai <world1955@hotmail.com>
2025-12-22 23:34:13 +0700

    added boolean tests

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -85,25 +85,75 @@ func TestNumberBinaryOperatorEvaluations(t *testing.T) {
 }
 
 func TestBinaryBooleanOperatorEvaluations(t *testing.T) {
-	// cases := []*EvaluationCase{
-	// 	{
-	// 		"true",
-	// 		"true",
-	// 		b.BOOLEAN_BOX,
-	// 	},
-	// }
-	// for _, c := range cases {
-	// 	eval := NewEvaluator(getDefaultCurrencyConverter(200))
+	cases := []*EvaluationCase{
+		{
+			"true",
+			"true",
+			b.BOOLEAN_BOX,
+		},
+		{
+			"false",
+			"false",
+			b.BOOLEAN_BOX,
+		},
+		{
+			"true && true",
+			"true",
+			b.BOOLEAN_BOX,
+		},
+		{
+			"true && false",
+			"false",
+			b.BOOLEAN_BOX,
+		},
+		{
+			"true || false",
+			"true",
+			b.BOOLEAN_BOX,
+		},
+		{
+			"false || false",
+			"false",
+			b.BOOLEAN_BOX,
+		},
+		{
+			"5 == 5",
+			"true",
+			b.BOOLEAN_BOX,
+		},
+		{
+			"5 != 5",
+			"false",
+			b.BOOLEAN_BOX,
+		},
+		{
+			"10 > 5",
+			"true",
+			b.BOOLEAN_BOX,
+		},
+		{
+			"3 <= 2",
+			"false",
+			b.BOOLEAN_BOX,
+		},
+		{
+			"(5 > 2) && (10 < 20)",
+			"true",
+			b.BOOLEAN_BOX,
+		},
+	}
+	for _, c := range cases {
+		eval := NewEvaluator(getDefaultCurrencyConverter(200))
 
-	// 	obj := eval.EvalLine(c.Line)
+		obj := eval.EvalLine(c.Line)
 
-	// 	if obj.Inspect() != c.ExpectPrint {
-	// 		t.Fatalf("Expected inspect result to be %s, got %s", c.ExpectPrint, obj.Inspect())
-	// 	}
-	// 	if obj.Type() != c.ExpectType {
-	// 		t.Fatalf("Expected identifier object, got %+v", obj.Type())
-	// 	}
-	// }
+		if obj.Inspect() != c.ExpectPrint {
+			t.Fatalf("Expected inspect result to be %s, got %s", c.ExpectPrint, obj.Inspect())
+		}
+		if obj.Type() != c.ExpectType {
+			t.Fatalf("Expected identifier object, got %+v", obj.Type())
+		}
+	}
 }
 
 func TestCurrencyEvaluation(t *testing.T) {
