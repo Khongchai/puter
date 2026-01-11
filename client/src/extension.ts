36281d25c0f70684092b08949079306d9f514df7
Khongchai <world1955@hotmail.com>
2026-01-11 14:49:43 +0700

    brought in client

---
+++ client/src/extension.ts
@@ -0,0 +1,67 @@
+import * as path from "path";
+import * as vscode from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient | undefined;
+
+export async function activate(context: vscode.ExtensionContext) {
+  const serverOptions = (() => {
+    const lsPath = path.resolve(context.extensionPath, "binaries");
+    const exePath = path.join(
+      lsPath,
+      `puter${process.platform === "win32" ? ".exe" : ""}`
+    );
+    const option: ServerOptions = {
+      // Note: if we can't find the package during build, take a look at this
+      // https://github.com/microsoft/typescript-go/blob/main/_packages/native-preview/lib/getExePath.js
+      command: exePath,
+      transport: TransportKind.stdio,
+    };
+    return option;
+  })();
+
+  const clientOptions: LanguageClientOptions = (() => {
+    const outputChannel = vscode.window.createOutputChannel("puter");
+    const traceOutputChannel =
+      vscode.window.createOutputChannel("puter (trace)");
+    return {
+      outputChannel,
+      traceOutputChannel,
+      documentSelector: [
+        {
+          scheme: "file",
+          language: "*",
+        },
+        {
+          scheme: "untitled",
+          language: "*",
+        },
+      ],
+    };
+  })();
+
+  client = new LanguageClient(
+    "puter",
+    "puter language server",
+    serverOptions,
+    clientOptions
+  );
+
+  await client.start();
+  await client.sendNotification("workspace/didChangeConfiguration", {
+    settings: {
+      "vscode-languageclient": {
+        trace: { server: "verbose" },
+      },
+    },
+  });
+}
+
+export async function deactivate() {
+  await client?.stop();
+}
