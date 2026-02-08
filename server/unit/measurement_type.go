a01ce08f3067feda9e3a65da12582b5039e8930e
Khongchai <world1955@hotmail.com>
2026-02-08 15:23:50 +0700

    all measurement types implemented

--- server/unit/measurement_type.go
+++ server/unit/measurement_type.go
@@ -20,7 +20,7 @@ type MeasurementDetail struct {
 }
 
 var MeasurementTypes = map[MeasurementType]*MeasurementDetail{
-	// Length - Metric
+	// Length - Metric (Base: mm)
 	"mm": {
 		Measures:     "length",
 		FullName:     "millimeters",
@@ -46,7 +46,7 @@ var MeasurementTypes = map[MeasurementType]*MeasurementDetail{
 		FromBaseUnit: func(value float64) float64 { return value / 10 / 100 / 1000 },
 	},
 
-	// Length - Imperial
+	// Length - Imperial (Base: mm)
 	"in": {
 		Measures:     "length",
 		FullName:     "inches",
@@ -72,23 +72,75 @@ var MeasurementTypes = map[MeasurementType]*MeasurementDetail{
 		FromBaseUnit: func(value float64) float64 { return value / 5280 / 12 / 25.4 },
 	},
 
-	// // Mass/Weight
-	// "mg":  "milligrams",
-	// "g":   "grams",
-	// "kg":  "kilograms",
-	// "lbs": "pounds",
-	// "ton": "tons",
+	// Mass/Weight (Base: mg)
+	"mg": {
+		Measures:     "mass",
+		FullName:     "milligrams",
+		ToBaseUnit:   func(value float64) float64 { return value },
+		FromBaseUnit: func(value float64) float64 { return value },
+	},
+	"g": {
+		Measures:     "mass",
+		FullName:     "grams",
+		ToBaseUnit:   func(value float64) float64 { return value * 1000 },
+		FromBaseUnit: func(value float64) float64 { return value / 1000 },
+	},
+	"kg": {
+		Measures:     "mass",
+		FullName:     "kilograms",
+		ToBaseUnit:   func(value float64) float64 { return value * 1000 * 1000 },
+		FromBaseUnit: func(value float64) float64 { return value / 1000 / 1000 },
+	},
+	// https://en.wikipedia.org/wiki/Avoirdupois#:~:text=This%20term%20originally%20referred%20to,used%20to%20weigh%20such%20merchandise.
+	// 1 lb = 453,592.37 mg
+	"lbs": {
+		Measures:     "mass",
+		FullName:     "pounds",
+		ToBaseUnit:   func(value float64) float64 { return value * 453592.37 },
+		FromBaseUnit: func(value float64) float64 { return value / 453592.37 },
+	},
+	"ton": {
+		Measures:     "mass",
+		FullName:     "tons", // Metric Tonne
+		ToBaseUnit:   func(value float64) float64 { return value * 1000 * 1000 * 1000 },
+		FromBaseUnit: func(value float64) float64 { return value / 1000 / 1000 / 1000 },
+	},
 
-	// // Volume
-	// "ml": "milliliters",
-	// "l":  "liters",
+	// Volume (Base: ml)
+	"ml": {
+		Measures:     "volume",
+		FullName:     "milliliters",
+		ToBaseUnit:   func(value float64) float64 { return value },
+		FromBaseUnit: func(value float64) float64 { return value },
+	},
+	"l": {
+		Measures:     "volume",
+		FullName:     "liters",
+		ToBaseUnit:   func(value float64) float64 { return value * 1000 },
+		FromBaseUnit: func(value float64) float64 { return value / 1000 },
+	},
 
-	// // Temperature
-	// "c": "celsius",
-	// "f": "fahrenheit",
-	// "k": "kelvin",
+	// Temperature (Base: Celsius)
+	"c": {
+		Measures:     "temperature",
+		FullName:     "celsius",
+		ToBaseUnit:   func(value float64) float64 { return value },
+		FromBaseUnit: func(value float64) float64 { return value },
+	},
+	"f": {
+		Measures:     "temperature",
+		FullName:     "fahrenheit",
+		ToBaseUnit:   func(value float64) float64 { return (value - 32) * 5 / 9 },
+		FromBaseUnit: func(value float64) float64 { return (value * 9 / 5) + 32 },
+	},
+	"k": {
+		Measures:     "temperature",
+		FullName:     "kelvin",
+		ToBaseUnit:   func(value float64) float64 { return value - 273.15 },
+		FromBaseUnit: func(value float64) float64 { return value + 273.15 },
+	},
 
-	// Time
+	// Time (Base: ms)
 	"ms": {
 		Measures:     "time",
 		FullName:     "milliseconds",
